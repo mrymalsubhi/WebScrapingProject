998d3339be509268825145e66e26b54dacd65c4b
Maryam • مريم <85639068+mrymalsubhi@users.noreply.github.com>
2024-05-11 21:30:37 +0300

    Delete cron_job.sh

--- cron_job.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Add the cron job to run scrape_clean.py every 10 minutes
-(crontab -l ; echo "*/10 * * * * /usr/bin/python3 /home/ubuntu/ArnoldAndSon/src/scrape_clean.py") | crontab -
